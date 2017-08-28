d1094064326f96855ccd37c42e81f6433aca5b11
Devesh Kumar Shrivastav <unidev39@gmail.com>
2017-08-28 11:19:19 +0545

    Update Partition_Over_Maxvalue.sql

--- Partition_Over_Maxvalue.sql
+++ Partition_Over_Maxvalue.sql
@@ -296,3 +296,280 @@ END pk_partition_over_maxvalue;
 /
 
 EXECUTE pk_partition_over_maxvalue.sp_partition_over_maxvalue ('<<Provide The Owner Name>>','<<Provide The Partition Table Name>>','<<Provide The Year>>');
+
+
+-- Under Anonymous Block --
+DECLARE
+    l_table_name           VARCHAR2(30) := 'split_partition';
+    l_table_owner          VARCHAR2(30) := 'hr';
+    l_identify_name_cur    sys_refcursor;
+    l_old_partition_name   VARCHAR2(50);
+    l_high_value           VARCHAR2(30);
+    l_flag                 VARCHAR2(10);
+    l_high_value_1         VARCHAR2(50);
+    l_high_value_2         VARCHAR2(50);
+    l_high_value_3         VARCHAR2(50);
+    flag_o                 VARCHAR2(50);
+    high_value_final_o     VARCHAR2(50);
+    old_partition_name_o   VARCHAR2(50);
+BEGIN
+    -- To segregate the partition value in three parts
+    pk_partition_over_maxvalue.sp_identify_partition_name(l_table_name,l_table_owner,l_identify_name_cur);
+    LOOP
+      FETCH l_identify_name_cur INTO l_old_partition_name,l_high_value,l_flag;
+      EXIT WHEN l_identify_name_cur%NOTFOUND;
+      IF (l_old_partition_name IS NOT NULL) THEN
+         old_partition_name_o := l_old_partition_name;
+      ELSIF (l_high_value IS NOT NULL) THEN
+          IF l_flag = 2 THEN
+             high_value_final_o := l_high_value;
+             l_high_value_1 := SUBSTR(l_high_value,2,8);
+          ELSIF l_flag = 3 THEN
+             l_high_value_2 := SUBSTR(l_high_value,2,8);
+          ELSIF l_flag = 4 THEN
+             l_high_value_3 := SUBSTR(l_high_value,2,8);
+          END IF;
+      END IF;
+    END LOOP;
+    CLOSE l_identify_name_cur;
+    Dbms_Output.Put_Line('*********************************************************************************************************************************');
+    dbms_output.put_line('Flagging Mechanism to identify the max Partition Value and Name => '||l_flag||' , '||high_value_final_o||' , '||old_partition_name_o);
+    Dbms_Output.Put_Line('*********************************************************************************************************************************'); 
+
+    -- To identify the nature of partition
+    BEGIN
+        l_high_value_1 := TO_DATE(l_high_value_1,'YYYYMMDD');
+        IF (SUBSTR(l_high_value_1,2,4) = SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) = SUBSTR(l_high_value_3,2,4)) THEN
+           l_flag := TO_DATE(l_high_value_1,'YYYYMMDD')-TO_DATE(l_high_value_2,'YYYYMMDD');
+        ELSIF (SUBSTR(l_high_value_1,2,4) = SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) <> SUBSTR(l_high_value_3,2,4)) THEN
+           l_flag := TO_DATE(l_high_value_1,'YYYYMMDD')-TO_DATE(l_high_value_2,'YYYYMMDD');
+        ELSIF (SUBSTR(l_high_value_1,2,4) <> SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) = SUBSTR(l_high_value_3,2,4)) THEN
+           l_flag := TO_DATE(l_high_value_2,'YYYYMMDD')-TO_DATE(l_high_value_3,'YYYYMMDD');
+        END IF;
+        flag_o := l_flag;
+    EXCEPTION WHEN OTHERS THEN
+       IF (LENGTH(l_high_value_1) >=9) THEN
+          IF (SUBSTR(l_high_value_1,2,4) = SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) = SUBSTR(l_high_value_3,2,4)) THEN
+             l_flag := l_high_value_1-l_high_value_2;
+          ELSIF (SUBSTR(l_high_value_1,2,4) = SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) <> SUBSTR(l_high_value_3,2,4)) THEN
+             l_flag := l_high_value_1-l_high_value_2;
+          ELSIF (SUBSTR(l_high_value_1,2,4) <> SUBSTR(l_high_value_2,2,4)) AND (SUBSTR(l_high_value_2,2,4) = SUBSTR(l_high_value_3,2,4)) THEN
+             l_flag := l_high_value_2-l_high_value_3;
+          END IF;
+          flag_o := l_flag;
+      ELSE
+         flag_o := '~';
+         old_partition_name_o := '';
+         high_value_final_o := '';
+      END IF;
+    END;
+    Dbms_Output.Put_Line('*********************************************************************************************************************************');
+    dbms_output.put_line('Flagging Mechanism to Identify the Nature of Partition => '||flag_o||' , '||high_value_final_o||' , '||old_partition_name_o);
+    Dbms_Output.Put_Line('*********************************************************************************************************************************');
+END ;
+/
+
+/*
+*********************************************************************************************************************************
+Flagging Mechanism to identify the max Partition Value and Name => 4 , 120180107000 , P_097                                      
+*********************************************************************************************************************************
+*********************************************************************************************************************************
+Flagging Mechanism to Identify the Nature of Partition          => 35 , 120180107000 , P_097                                              
+*********************************************************************************************************************************                     
+*/
+
+DECLARE
+    l_table_name           VARCHAR2(30) := 'split_partition';
+    l_table_owner          VARCHAR2(30) := 'hr';
+    l_column_name_year     VARCHAR2(10) := '2018';
+    l_old_partition_name   VARCHAR2(50);
+    l_new_partition_name   VARCHAR2(50);
+    l_high_value           VARCHAR2(30);
+    l_flag                 VARCHAR2(10);
+    l_sql                  VARCHAR2(32767);
+    l_column               VARCHAR2(100);
+    l_group_by             VARCHAR2(100) := '';
+    l_operator             VARCHAR2(10) := 'AND ';
+    TYPE l_sql_curr        IS REF CURSOR;
+    l_curr                 l_sql_curr;
+    l_new_partition_value  VARCHAR2(100);
+    l_table_name_lookup    VARCHAR2(30) := '';
+BEGIN
+      pk_partition_over_maxvalue.sp_identify_partition_type(l_table_name,l_table_owner,l_flag,l_high_value,l_old_partition_name);
+      BEGIN
+           IF (l_flag <> '~') THEN 
+              -- To find existing type of partition in Our Database
+              IF l_flag >= 28 THEN 
+                 l_column := 'TO_CHAR(''1''||a.column_name_1||''000'') ';
+                 l_table_name_lookup := 'table_name_1';
+              ELSIF l_flag = 7 THEN 
+                 l_column := 'TO_CHAR(''1''||a.column_name_2||''000'') ';
+                 l_table_name_lookup := 'table_name_2';
+              ELSIF l_flag IN (4,5) THEN
+                 l_column := 'TO_CHAR(MIN(a.column_name_3)) ';
+                 l_table_name_lookup := 'table_name_2';
+                 l_operator := 'HAVING ';
+                 l_group_by := 'GROUP BY a.column_name_4 ';
+              ELSIF l_flag = 1 THEN
+                 l_column := 'TRIM(TO_CHAR(a.column_name_3)) ';
+                 l_table_name_lookup := 'table_name_2';
+              END IF;
+              
+              l_sql := 'SELECT
+                             c.new_partition_value,
+                             CASE
+                                WHEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',- 1)-1) = ''P'' THEN 
+                                CASE 
+                                   WHEN LENGTH(c.new_partition_name) = 1 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_00''||c.new_partition_name
+                                   WHEN LENGTH(c.new_partition_name) = 2 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_0''||c.new_partition_name
+                                   WHEN LENGTH(c.new_partition_name) = 3 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_''||c.new_partition_name
+                                END
+                             ELSE 
+                                CASE
+                                   WHEN LENGTH(c.new_partition_name) = 1 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_P00''||c.new_partition_name
+                                   WHEN LENGTH(c.new_partition_name) = 2 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_P0''||c.new_partition_name
+                                   WHEN LENGTH(c.new_partition_name) = 3 THEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1)||''_P''||c.new_partition_name
+                                END
+                             END new_partition_name
+                         FROM (
+                               SELECT
+                                    b.new_partition_value         new_partition_value,
+                                    b.new_partition_name + ROWNUM new_partition_name
+                               FROM (
+                                     SELECT DISTINCT
+                                          '||l_column||' new_partition_value,
+                                          CASE 
+                                             WHEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',-1)-1) = ''P'' THEN SUBSTR('''||l_old_partition_name||''',INSTR('''||l_old_partition_name||''',''_'',-1)+1)
+                                             WHEN SUBSTR('''||l_old_partition_name||''',1,INSTR('''||l_old_partition_name||''',''_'',- 1)+1) LIKE ''%/_P'' ESCAPE ''/'' THEN SUBSTR('''||l_old_partition_name||''',INSTR('''||l_old_partition_name||''',''_'',-1)+2)
+                                          END new_partition_name
+                                     FROM 
+                                          '||l_table_name_lookup||' a
+                                     WHERE 
+                                          TRIM(a.column_name_year) = '''||l_column_name_year||'''
+                                     '||l_operator||' '||l_column||' > '''||l_high_value||'''
+                                     AND NOT EXISTS (
+                                                     SELECT
+                                                          1
+                                                     FROM 
+                                                          all_tab_partition b
+                                                     WHERE
+                                                         ('||l_column||' = TRIM(TO_CHAR(b.high_value)))
+                                                    )
+                                     '||l_group_by||'
+                                     ORDER BY new_partition_value ASC
+                                   ) b
+                             ) c';
+              
+              OPEN l_curr FOR l_sql;
+              LOOP
+                 FETCH l_curr INTO l_new_partition_value,l_new_partition_name;
+                 EXIT WHEN l_curr%NOTFOUND;
+                 BEGIN
+                     IF (l_new_partition_value IS NOT NULL AND l_new_partition_name IS NOT NULL) THEN
+                         l_flag := 1;
+                         IF (l_flag = 1) THEN 
+                            dbms_output.put_line('ALTER TABLE '||l_table_owner||'.'||l_table_name||' SPLIT PARTITION '||l_old_partition_name||' AT ('||l_new_partition_value||')
+                                                  INTO 
+                                                  (
+                                                   PARTITION '||l_new_partition_name||', 
+                                                   PARTITION '||l_old_partition_name||'
+                                                  );');
+                            l_old_partition_name := l_new_partition_name;
+                         ELSE
+                            dbms_output.put_line('ALTER TABLE '||l_table_owner||'.'||l_table_name||' SPLIT PARTITION '||l_new_partition_name||' AT ('||l_new_partition_value||')
+                                                  INTO 
+                                                  (
+                                                   PARTITION '||l_new_partition_name||', 
+                                                   PARTITION '||l_old_partition_name||'
+                                                  );');
+                            l_old_partition_name := l_new_partition_name;
+                         END IF;
+                      ELSE
+                         dbms_output.put_line('Please update the Lookup Table !!!!!');
+                      END IF;
+                 EXCEPTION WHEN OTHERS THEN 
+                    dbms_output.put_line(SQLERRM);
+                 END;
+              END LOOP;
+              CLOSE l_curr;
+          ELSE 
+             dbms_output.put_line('Please Provide the Valied Partition Table Name Or Owner Name');
+          END IF;
+      END;
+END;
+/
+
+/*                                       
+ALTER TABLE hr.split_partition SPLIT PARTITION P_097 AT (120180128000)
+INTO 
+(
+ PARTITION P_098, 
+ PARTITION P_097
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_098 AT (120180225000)
+INTO 
+(
+ PARTITION P_099, 
+ PARTITION P_098
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_099 AT (120180401000)
+INTO 
+(
+ PARTITION P_100, 
+ PARTITION P_099
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_100 AT (120180429000)
+INTO 
+(
+ PARTITION P_101, 
+ PARTITION P_100
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_101 AT (120180527000)
+INTO 
+(
+ PARTITION P_102, 
+ PARTITION P_101
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_102 AT (120180701000)
+INTO 
+(
+ PARTITION P_103, 
+ PARTITION P_102
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_103 AT (120180729000)
+INTO 
+(
+ PARTITION P_104, 
+ PARTITION P_103
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_104 AT (120180826000)
+INTO 
+(
+ PARTITION P_105, 
+ PARTITION P_104
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_105 AT (120180930000)
+INTO 
+(
+ PARTITION P_106, 
+ PARTITION P_105
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_106 AT (120181028000)
+INTO 
+(
+ PARTITION P_107, 
+ PARTITION P_106
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_107 AT (120181125000)
+INTO 
+(
+ PARTITION P_108, 
+ PARTITION P_107
+);
+ALTER TABLE hr.split_partition SPLIT PARTITION P_108 AT (120181230000)
+INTO 
+(
+ PARTITION P_109, 
+ PARTITION P_108
+);
+*/
