d419dda9461fe95227601d85658a4d39007410d6
Devesh Kumar Shrivastav <unidev39@gmail.com>
2017-11-21 10:14:11 +0545

    Update Alter_Oracle_User.sql

--- Alter_Oracle_User.sql
+++ Alter_Oracle_User.sql
@@ -6,33 +6,33 @@ ALTER USER <<user_name>> IDENTIFIED BY <<New_Password>> REPLACE <<Old_Password>>
 -- Step 1: Login from SYS privilege user, To Find the existing password in encrypted form
 CONNECT <<sys_user>>/<<sys_password>> AS SYSDBA
  
-SELECT name, password FROM sys.user$ WHERE name = 'HR';
+SELECT name, password FROM sys.user$ WHERE name = '<<user_name>>';
 
 /*
-NAME PASSWORD
----- ----------------        
-HR 5F6008F9ED270AD6
+NAME          PASSWORD
+------------- ----------------        
+<<user_name>> 5F6008F9ED270AD6
 */
 
---To assign the temp password for user - HR
-ALTER USER HR IDENTIFIED BY <<temp_password>>;
+--To assign the temp password for user - <<user_name>>
+ALTER USER <<user_name>> IDENTIFIED BY <<temp_password>>;
 
 --Step 2: Connect to the HR user with password
-CONNECT hr/<<hr_temp_password>> 
+CONNECT <<user_name>>/<<hr_temp_password>> 
 
 SELECT USER FROM dual;
 /*
 USER
 ----
-HR 
+<<user_name>> 
 */
 
--- Step 3: Disconnect from HR user
+-- Step 3: Disconnect from <<user_name>> user
 
 -- Step 4: Agian Login from SYS privilege user
 CONNECT <<sys_user>>/<<sys_password>> AS SYSDBA
 
-ALTER USER hr IDENTIFIED BY VALUES '5F6008F9ED270AD6';
+ALTER USER <<user_name>> IDENTIFIED BY VALUES '5F6008F9ED270AD6';
 
 -- Step 5: The password is now reset as it was.
 -- Note: Please noted down the encrypted as it was.
