f10dc49b876a228e344ccd5f8a656bf04a5be648
Devesh Kumar Shrivastav <unidev39@gmail.com>
2017-12-14 11:20:53 +0545

    Update pk_partition_over_maxvalue.sql

--- pk_partition_over_maxvalue.sql
+++ pk_partition_over_maxvalue.sql
@@ -1,13 +1,12 @@
--- Step 1 --
 CREATE OR REPLACE PACKAGE ra_data_mart.pk_partition_over_maxvalue
 AS
-  -- To identify the exiting partition name and max value 
+  -- To identify the exiting partition name and max value
   PROCEDURE sp_identify_partition_name
   (
     p_table_name             VARCHAR2,
     p_table_owner            VARCHAR2
   );
-  -- To identify the nature of partition type 
+  -- To identify the nature of partition type
   PROCEDURE sp_identify_partition_type
   (
     p_table_name              VARCHAR2,
@@ -17,7 +16,7 @@ AS
     old_partition_name_o  OUT VARCHAR2
   );
   -- To Split the Partition Over Max Value - Single Table
-  PROCEDURE sp_partition_over_maxvalue    
+  PROCEDURE sp_partition_over_maxvalue
   (
     p_table_owner            VARCHAR2,
     p_table_name             VARCHAR2,
@@ -32,11 +31,10 @@ AS
 END pk_partition_over_maxvalue;
 /
 
--- Step 2 --
 CREATE OR REPLACE PACKAGE BODY ra_data_mart.pk_partition_over_maxvalue
 AS
   -- To identify the exiting partition name and max value
-  PROCEDURE sp_identify_partition_name  
+  PROCEDURE sp_identify_partition_name
   (
     p_table_name             VARCHAR2,
     p_table_owner            VARCHAR2
@@ -145,7 +143,7 @@ AS
            END IF;
       END LOOP;
       CLOSE l_curr;
-  
+
       -- To identify the nature of partition
       BEGIN
           l_high_value_1 := TO_DATE(l_high_value_1,'YYYYMMDD');
@@ -182,7 +180,7 @@ AS
     p_table_name             VARCHAR2,
     p_mcal_year              VARCHAR2
   )
-  AS  
+  AS
      l_table_name           VARCHAR2(30) := UPPER(p_table_name);
      l_mcal_year            VARCHAR2(10) := p_mcal_year;
      l_table_owner          VARCHAR2(30) := UPPER(p_table_owner);
@@ -203,19 +201,19 @@ AS
      l_row_count            PLS_INTEGER;
      l_validate_user        NUMBER;
   BEGIN
-      l_sql := 'SELECT 
-                     COUNT(table_owner) 
-                FROM 
-                     all_tab_partitions 
-                WHERE 
+      l_sql := 'SELECT
+                     COUNT(table_owner)
+                FROM
+                     all_tab_partitions
+                WHERE
                     table_owner = '''||l_table_owner||''' ';
       EXECUTE IMMEDIATE l_sql INTO l_validate_user;
 
-      IF (l_validate_user = 0) THEN 
+      IF (l_validate_user = 0) THEN
           dbms_output.put_line('Please Provide the Valied Owner Name '||l_table_owner||'  for Year '''||l_mcal_year||''' !!!!!');
       ELSE
           ra_data_mart.pk_partition_over_maxvalue.sp_identify_partition_type(l_table_name,l_table_owner,l_flag,l_high_value,l_old_partition_name);
-          
+
           IF (l_old_partition_name = '~') THEN
               dbms_output.put_line('The table '||l_table_name||' has not been partition over MAXVALUE  for Year '''||l_mcal_year||''' ');
           ELSE
@@ -227,10 +225,10 @@ AS
                          l_sql := 'SELECT
                                         owner,
                                         table_name
-                                   FROM 
+                                   FROM
                                         all_tables
-                                   WHERE 
-                                       table_name = UPPER(''w_mcal_day_d'') ';
+                                   WHERE
+                                       table_name = UPPER(''w_mcal_day_d'')';
                          EXECUTE IMMEDIATE l_sql INTO l_table_owner_lookup_1,l_table_name_lookup;
                       ELSE
                          l_sql := 'SELECT
@@ -238,16 +236,16 @@ AS
                                         table_name
                                    FROM
                                         all_tables
-                                   WHERE 
-                                       table_name = UPPER(''w_mcal_week_d'') ';
+                                   WHERE
+                                       table_name = UPPER(''w_mcal_week_d'')';
                          EXECUTE IMMEDIATE l_sql INTO l_table_owner_lookup_2,l_table_name_lookup;
                       END IF;
 
                       -- To find existing type of partition in Our Database --
-                      IF l_flag >= 28 THEN 
+                      IF l_flag >= 28 THEN
                          l_column := 'TO_CHAR(''1''||a.mcal_period_start_dt_wid||''000'') ';
                          l_table_name_lookup := l_table_owner_lookup_1||'.'||l_table_name_lookup;
-                      ELSIF l_flag = 7 THEN 
+                      ELSIF l_flag = 7 THEN
                          l_column := 'TO_CHAR(''1''||a.mcal_week_start_dt_wid||''000'') ';
                          l_table_name_lookup := l_table_owner_lookup_2||'.'||l_table_name_lookup;
                       ELSIF l_flag IN (4,5) THEN
@@ -259,7 +257,7 @@ AS
                          l_column := 'TRIM(TO_CHAR(a.row_wid)) ';
                          l_table_name_lookup := l_table_owner_lookup_2||'.'||l_table_name_lookup;
                       END IF;
-              
+
                       l_sql := 'SELECT
                                      c.new_partition_value,
                                      CASE
@@ -278,7 +276,7 @@ AS
                                                 WHEN Length(REGEXP_SUBSTR(new_partition_name,''[0-9]+'')) = 3
                                                 THEN regexp_replace('''||l_old_partition_name||''',''[0-9]'')||REGEXP_SUBSTR(new_partition_name,''[0-9]+'')
                                                 ELSE regexp_replace('''||l_old_partition_name||''',''[0-9]'')||''0''||REGEXP_SUBSTR(new_partition_name,''[0-9]+'') END
-                                     END new_partition_name 
+                                     END new_partition_name
                                  FROM (
                                        SELECT
                                             b.new_partition_value         new_partition_value,
@@ -287,15 +285,15 @@ AS
                                              SELECT DISTINCT
                                                   '||l_column||'                                                               new_partition_value,
                                                   To_Char(TO_NUMBER(REGEXP_SUBSTR('''||l_old_partition_name||''',''[0-9]+''))) new_partition_name
-                                             FROM 
+                                             FROM
                                                   '||l_table_name_lookup||' a
-                                             WHERE 
+                                             WHERE
                                                   TRIM(a.mcal_year) <= '''||l_mcal_year||'''
                                              '||l_having||' '||l_column||' > '''||l_high_value||'''
                                              AND NOT EXISTS (
                                                              SELECT
                                                                   1
-                                                             FROM 
+                                                             FROM
                                                                   '||l_table_owner||'.all_tab_partition b
                                                              WHERE
                                                                  ('||l_column||' = TRIM(TO_CHAR(b.high_value)))
@@ -309,19 +307,19 @@ AS
                          FETCH l_curr INTO l_new_partition_value,l_new_partition_name;
                          EXIT WHEN l_curr%NOTFOUND;
                            BEGIN
-                               dbms_output.put_line('ALTER TABLE '||l_table_owner||'.'||l_table_name||' SPLIT PARTITION '||l_old_partition_name||' AT ('||l_new_partition_value||')
-                                                     INTO
-                                                     (
-                                                      PARTITION '||l_new_partition_name||',
-                                                      PARTITION '||l_old_partition_name||'
-                                                     );');
+                              EXECUTE IMMEDIATE 'ALTER TABLE '||l_table_owner||'.'||l_table_name||' SPLIT PARTITION '||l_old_partition_name||' AT ('||l_new_partition_value||')
+                                                 INTO
+                                                 (
+                                                  PARTITION '||l_new_partition_name||',
+                                                  PARTITION '||l_old_partition_name||'
+                                                 )';
                                l_row_count := l_curr%ROWCOUNT;
                            EXCEPTION WHEN OTHERS THEN
                                 dbms_output.put_line(SQLERRM);
                            END;
                       END LOOP;
                       CLOSE l_curr;
-                      
+
                       IF l_row_count IS NULL THEN
                          IF l_flag >= 28 THEN
                             dbms_output.put_line('Please Update the Lookup Table '||l_table_name_lookup||' for Year '''||l_mcal_year||''' !!!!!');
@@ -329,58 +327,58 @@ AS
                             dbms_output.put_line('Please Update the Lookup Table '||l_table_name_lookup||' for Year '''||l_mcal_year||''' !!!!!');
                          END IF;
                       ELSE
+                         -- To Rename the Partition 
                          BEGIN
                              l_sql := 'SELECT
                                             table_owner        table_owner,
                                             table_name         table_name,
-                                            partition_name     old_partition_name,
-                                            CASE 
-                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 1 
-                                               THEN CASE 
-                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 1 
+                                            CASE
+                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 1
+                                               THEN CASE
+                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 1
                                                        THEN regexp_replace(partition_name,''[0-9]'')||''00''||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1)
                                                        ELSE regexp_replace(partition_name,''[0-9]'')||''0''||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) END
-                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 2 
-                                               THEN CASE 
-                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 2 
+                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 2
+                                               THEN CASE
+                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 2
                                                        THEN regexp_replace(partition_name,''[0-9]'')||''0''||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1)
                                                        ELSE regexp_replace(partition_name,''[0-9]'')||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) END
-                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 3 
-                                               THEN CASE 
-                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 3 
-                                                       THEN regexp_replace(partition_name,''[0-9]'')||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) 
+                                               WHEN Length(To_Number(REGEXP_SUBSTR(partition_name,''[0-9]+''))) = 3
+                                               THEN CASE
+                                                       WHEN Length(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) = 3
+                                                       THEN regexp_replace(partition_name,''[0-9]'')||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1)
                                                        ELSE regexp_replace(partition_name,''[0-9]'')||''0''||To_Char(TO_NUMBER(REGEXP_SUBSTR(partition_name,''[0-9]+''))+1) END
                                             END new_partition_name
                              FROM
                                   all_tab_partitions a
                              WHERE
-                                  1 = (
-                                       SELECT 
+                                  2 = (
+                                       SELECT
                                            COUNT(DISTINCT partition_position)
-                                       FROM 
+                                       FROM
                                            all_tab_partitions b
-                                       WHERE 
+                                       WHERE
                                            a.partition_position <= b.partition_position
                                        AND  a.table_name = b.table_name
                                        AND  a.table_name = UPPER('''||l_table_name||''')
                                       )';
-                             EXECUTE IMMEDIATE (l_sql) INTO l_table_owner,l_table_name,l_old_partition_name,l_new_partition_name;
-                             BEGIN 
-                                 dbms_output.put_line('ALTER TABLE '||l_table_owner||'.'||l_table_name||' RENAME PARTITION '||l_old_partition_name||' TO '||l_new_partition_name||' ;');
+                             EXECUTE IMMEDIATE (l_sql) INTO l_table_owner,l_table_name,l_new_partition_name;
+                             BEGIN
+                                 EXECUTE IMMEDIATE 'ALTER TABLE '||l_table_owner||'.'||l_table_name||' RENAME PARTITION '||l_old_partition_name||' TO '||l_new_partition_name;
                                  dbms_output.put_line('The Table '||l_table_owner||'.'||l_table_name||' has been Successfully Split Over MAXVALUE for Year '''||l_mcal_year||''' !!!!!');
                                  EXCEPTION WHEN OTHERS THEN
                                  dbms_output.put_line(SQLERRM);
                              END;
                          END;
                       END IF;
-                  ELSE 
+                  ELSE
                      dbms_output.put_line('Please Provide the Valied Partition Table Name '||l_table_name||' for Year '''||l_mcal_year||''' !!!!!');
                   END IF;
               END;
           END IF;
       END IF;
   END sp_partition_over_maxvalue;
-  
+
   -- To Split the Partition Over Max Value - Multiple Tables
   PROCEDURE sp_partition_over_maxvalues
   (
@@ -390,7 +388,7 @@ AS
   AS
      l_table_owner            VARCHAR2(30);
      l_table_name             VARCHAR2(30);
-     l_sql                    VARCHAR2(32767); 
+     l_sql                    VARCHAR2(32767);
      TYPE l_sql_curr          IS REF CURSOR;
      l_curr                   l_sql_curr;
   BEGIN
@@ -404,16 +402,16 @@ AS
                 AND regexp_like(table_name,''(^W_)'')
                 AND NOT regexp_like(table_name,''(^W_RTL_ITEM_GRP1_D|^W_RTL_ITEM_GRP2_D)'')';
 
-        -- To List of the Multiple Tables 
+        -- To List of the Multiple Tables
         OPEN l_curr FOR l_sql;
-        LOOP       
+        LOOP
           FETCH l_curr INTO l_table_owner,l_table_name;
           EXIT WHEN l_curr%NOTFOUND;
              BEGIN
                  ra_data_mart.pk_partition_over_maxvalue.sp_partition_over_maxvalue(l_table_owner,l_table_name,p_mcal_year);
-             EXCEPTION WHEN OTHERS THEN 
+             EXCEPTION WHEN OTHERS THEN
                  Dbms_Output.Put_Line(SQLERRM);
-             END; 
+             END;
         END LOOP;
         CLOSE l_curr;
   END sp_partition_over_maxvalues;
